7adf1dbb91414891af4026a8aa435e11c96c2f6b
Andreas Steffen <Andreas.Steffen@gmail.com>
2017-01-05 21:17:16 +0100

    added an example service

---
+++ script/symbol2entrez.py
@@ -0,0 +1,14 @@
+import click
+import mygene as mg
+#import pandas
+
+@click.command()
+@click.option('symbols', '--symbol', multiple=True)
+def convert(symbols):
+    mv = mg.MyGeneInfo()
+    result = mv.querymany(['CDK2','BRAF','BRD4'], scopes = 'symbol', fields='symbol,entrezgene', as_dataframe= True, species=9606, verbose = False)
+    result = result[['entrezgene','symbol']].to_json(orient = 'records')
+    click.echo(result)
+
+if __name__ == '__main__':
+    convert()
